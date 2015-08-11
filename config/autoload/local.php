100c0180aa5ff5b566e986b9ebecedfa18b5ff85
root <root@ip-172-31-12-80.ec2.internal>
2015-08-11 17:15:21 +0000

    added local.php changes

---
+++ config/autoload/local.php
@@ -0,0 +1,16 @@
+<?php
+#define('DB_USER_NAME', 'rootpr0d');
+#define('DB_PASSWORD', 'audit098$');
+#define('DB_NAME', 'auditcompanion');
+#define('DB_HOST', 'auditcompanionprod.c9cu0nitxfty.us-east-1.rds.amazonaws.com');
+#define('DB_PORT', '3306');
+#putenv('APPLICATION_ENV=production');
+
+define('DB_USER_NAME', 'pr0droot');
+define('DB_PASSWORD', 'aud1tc0mpan10n098');
+define('DB_NAME', 'auditcompanion');
+define('DB_HOST', 'auditcompanionprod.ch8u7og6vrzm.us-east-1.rds.amazonaws.com');
+define('DB_PORT', '3306');
+putenv('APPLICATION_ENV=production');
+
+return array();
